18d71cd7e10f87215c033e5f5a62ad15bc53eb87
bjoern <git@wotanii.de>
2020-05-23 15:47:19 +0200

    include entire config in results

--- resuts_to_csv.py
+++ resuts_to_csv.py
@@ -17,6 +17,7 @@ def read_simulations(base_directory):
         except:
             conf = None
             logging.error("couldn't read conf for " + str(simulation_folder), exc_info=True)
+            
         # noinspection PyBroadException
         try:
             with open(os.path.join(simulation_folder, 'Log.json'), 'r') as read_file:
@@ -39,29 +40,38 @@ def get_attribute_or_none(d, attr):
     return None
 
 
+def walk_dict(node, callback_node, depth=0):
+    for key, item in node.items():
+        if isinstance(item, dict):
+            callback_node(key, item, depth, False)
+            walk_dict(item, callback_node, depth + 1)
+        else:
+            callback_node(key, item, depth, True)
+
+
 def gather_info_for_csv(simulation):
     log = simulation["log"]
     conf = simulation["conf"]
     generations = [i for i in range(len(log))]
     avg = [generation["avg"] for generation in log]
     maximum = [generation["max"] for generation in log]
+    brain = conf["brain"]
+    trainer = conf["trainer"]
+    episode_runner = conf["episode_runner"]
+    del conf["brain"]
+    del conf["trainer"]
+    del conf["episode_runner"]
 
-    return {"environment": get_attribute_or_none(conf, "environment"),
-            "neural_network_type": get_attribute_or_none(conf, "neural_network_type"),
-            "trainer_type": get_attribute_or_none(conf, "trainer_type"),
-            "number_neurons": get_attribute_or_none(conf, "number_neurons"),
-            "population_size": get_attribute_or_none(conf, "population_size"),
-            "parameter_perturbations": get_attribute_or_none(conf, "parameter_perturbations"),
-            "gen": str(max(generations)),
+    return {"gen": str(max(generations)),
             "mavg": str(max(avg)),
             "max": str(max(maximum)),
-            "directory": simulation["dir"]}
+            "directory": simulation["dir"], **conf, **brain, **trainer, **episode_runner}
 
 
 logging.basicConfig()
 parser = argparse.ArgumentParser(description='Visualize experiment results')
 parser.add_argument('--dir', metavar='dir', type=str, help='base directory for input',
-                    default='Simulation_Results/CTRNN')
+                    default='data')
 parser.add_argument('--csv', metavar='type', type=str, help='location of output csv file', default='output.csv')
 args = parser.parse_args()
 
@@ -75,3 +85,4 @@ with open(args.csv, 'w') as output_file:
     dict_writer = csv.DictWriter(output_file, keys)
     dict_writer.writeheader()
     dict_writer.writerows(data)
+    logging.info("log written to " + str(args.csv))
